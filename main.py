5a603a8285c6c8ea08eb613b04b5eda71c220687
Julien Lebunetel <julien@lebunetel.com>
2021-10-02 00:34:40 +0200

    Add 'ddns' endpoint

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from fastapi import FastAPI, Header, Request
+import json
+import os
+import requests
+from fastapi import FastAPI, Header, HTTPException, Request
 from typing import Optional
+
+if os.path.exists(".env"):
+    with open(".env") as f:
+        for line in f:
+            key, value = line.strip().split("=", 1)
+            os.environ.setdefault(key, value)
+
+APIKEY = str(os.environ["ALWAYSDATA_APIKEY"])
+ACCOUNT = str(os.environ["ALWAYSDATA_ACCOUNT"])
+DDNS_TOKEN = str(os.environ["DDNS_TOKEN"])
 
 app = FastAPI()
 
@@ -16,4 +29,63 @@ async def showmyip(
     # address, see: https://help.alwaysdata.com/en/sites/http-stack/
     x_real_ip: Optional[str] = Header(None),
 ):
-    return {"ip": x_real_ip if x_real_ip else request.client.host}
+    """Returns my IP address"""
+    ip = x_real_ip if x_real_ip else request.client.host
+    return {"ip": ip}
+
+
+@app.get("/api/v1/ddns/")
+async def ddns(
+    request: Request,
+    # alwaysdata add to headers X-Real-IP, which takes the value of the client’s IP
+    # address, see: https://help.alwaysdata.com/en/sites/http-stack/
+    x_real_ip: Optional[str] = Header(None),
+    domain: int = 0,
+    record: int = 0,
+    name: str = "home",
+    ip: str = "",
+    token: str = "",
+):
+    """Sets my DDNS"""
+    # https://api.alwaysdata.com/v1/record/doc/
+
+    if token != DDNS_TOKEN:
+        raise HTTPException(status_code=403, detail="token not valid!")
+
+    if not domain:
+        raise HTTPException(status_code=422, detail="domain not valid!")
+
+    if not record:
+        raise HTTPException(status_code=422, detail="record not valid!")
+
+    address = "https://api.alwaysdata.com/v1/record/{record}/".format(record=record)
+
+    credentials = (
+        "{apikey} account={account}".format(apikey=APIKEY, account=ACCOUNT),
+        "",
+    )
+
+    if not ip:
+        if x_real_ip:
+            ip = x_real_ip
+        else:
+            ip = request.client.host
+
+    data = {
+        "domain": str(domain),
+        "type": "A",
+        "name": name,
+        "value": ip,
+    }
+
+    r = requests.put(
+        address,
+        auth=credentials,
+        data=json.dumps(data),
+    )
+
+    return {
+        "name": name,
+        "type": "A",
+        "value": ip,
+    }
